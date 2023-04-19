9f7de1ce554f6b474dfabef7d86221fa3f8cd498
Varun-SecurDI <124686864+Varun-SecurDI@users.noreply.github.com>
2023-04-19 19:13:55 +0530

    Add files via upload

---
+++ com/second/accountclaim/secondtest/SecondtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.second.accountclaim.secondtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
